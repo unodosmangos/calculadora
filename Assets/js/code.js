47f08a6e2ebc58e28b4b16e22ed015894e840a3f
unodosmangos <morellicj@yahoo.com>
2023-01-19 18:17:20 -0500

    avance calculadora 2

--- Assets/js/code.js
+++ Assets/js/code.js
@@ -1,4 +1,4 @@
-let calculdora = [{
+let calculadoraControl = [{
         tipo: "control",
         boton: "AC"
     },
@@ -11,7 +11,9 @@ let calculdora = [{
         tipo: "control",
         boton: '<i class="fa-solid fa-percent"></i>',
         valor: "%"
-    },
+    }]
+
+let calculadoraOperacion = [
     {
         tipo: "operacion",
         boton: '<i class="fa-solid fa-divide"></i>',
@@ -24,70 +26,84 @@ let calculdora = [{
     },
     {
         tipo: "operacion",
-        boton: <i class="fa-solid fa-minus"></i>,
+        boton: '<i class="fa-solid fa-minus"></i>',
         valor: "-"
     },
     {
         tipo: "operacion",
-        boton: <i class="fa-solid fa-plus"></i>,
+        boton: '<i class="fa-solid fa-plus"></i>',
         valor: "+",
-    },
+    }]
+
+let calculadoraNumeros = [
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-1"></i>,
+        boton: '<i class="fa-solid fa-1"></i>',
         valor: 1
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-2"></i>,
+        boton: '<i class="fa-solid fa-2"></i>',
         valor: 2
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-3"></i>,
+        boton: '<i class="fa-solid fa-3"></i>',
         valor: 3
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-4"></i>,
+        boton: '<i class="fa-solid fa-4"></i>',
         valor: 4
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-5"></i>,
+        boton: '<i class="fa-solid fa-5"></i>',
         valor: 5
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-6"></i>,
+        boton: '<i class="fa-solid fa-6"></i>',
         valor: 6
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-7"></i>,
+        boton: '<i class="fa-solid fa-7"></i>',
         valor: 7
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-8"></i>,
+        boton: '<i class="fa-solid fa-8"></i>',
         valor: 8
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-9"></i>,
+        boton: '<i class="fa-solid fa-9"></i>',
         valor: 9
     },
     {
         tipo: "numero",
-        boton: <i class="fa-solid fa-0"></i>,
+        boton: '<i class="fa-solid fa-0"></i>',
         valor: 0
     }
 ]
 
 let controlValor = document.getElementById('control')
 
-for (const botonesControl of calculadora) {
+for (const botonesControl of calculadoraControl) {
     controlValor.innerHTML += `
-        <button>${calculdora.boton}</button>
+        <div>
+        <button>${botonesControl.boton}</button>
+        </div>
     `
+}
+
+
+function sumar() {
+    let input = document.getElementById('numeroAsumar').value
+    let resultado = 0
+    for (let i = 1; i <= input; i++) {
+        resultado = i + resultado
+    }
+    document.getElementById('resultado').innerText = resultado
 }
